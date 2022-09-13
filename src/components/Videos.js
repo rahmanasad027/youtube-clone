e8ef04e7d275451d0dfb0d4f5f3314d5f3be2f2e
rahmanasad222 <asadur.rehman@cheetay.pk>
2022-09-13 00:49:28 -0700

    final push

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -2,10 +2,15 @@ import React from "react";
 import { Stack, Box } from "@mui/material";
 import { ChannelCard, VideoCard } from "../components";
 
-const Videos = ({ videos }) => {
+const Videos = ({ videos, direction }) => {
   // console.log("videos: ", videos);
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+    <Stack
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      gap={2}
+    >
       {videos.map((item, index) => (
         <Box key={index}>
           {item.id.videoId && <VideoCard video={item} />}
